0747dcbf8bb88e82d9de976ca19c712140d7d84f
HP <HP@LAPTOP-9U6BEHI8>
2025-10-28 12:34:12 +0530

    java

---
+++ corejavacode/src/com/tnsif/dayeigth/interfaces/extendinginterfaces/ChildInterface.java
@@ -0,0 +1,7 @@
+
+//child interface inherits from InterfaceOne//
+package com.tnsif.dayeight.interfaces.extendinginterfaces;
+
+public interface ChildInterface extends InterfaceOne {
+	void show();
+}
---
+++ corejavacode/src/com/tnsif/daynineteen/IsOdd.java
@@ -0,0 +1,8 @@
+
+//Program to demonstrate Functional Interface
+package com.tnsif.daynineteen;
+
+@FunctionalInterface
+interface IsOdd{
+	public boolean CheckOdd(int a);
+}
