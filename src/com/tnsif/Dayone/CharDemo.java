583f1fede1e58088d667aab84165c1afcf020c65
kasirajakasinathan <kasirajakasinathan@SINDUJA>
2025-10-28 21:02:08 +0530

    java program

---
+++ src/com/tnsif/Dayone/CharDemo.java
@@ -0,0 +1,26 @@
+
+//program to demonstrate char data type
+package com.tnsif.Dayone;
+public class CharDemo {
+	public static void main(String[] args) {
+		// assigning single character literal
+		char ch = 'b';
+		System.out.println(ch);
+
+		// assigning number to char
+		char ch1 = 64;
+		System.out.println(ch1);
+
+		// assigning unicode to char
+		char var1 = '\u00A7';
+		System.out.println(var1);
+
+		// Unicode representation
+		char var2 = '\u20AC';
+		System.out.println(var2);
+
+		// ASCI code representation
+		int a = 'B';
+		System.out.println(a);
+	}
+}
